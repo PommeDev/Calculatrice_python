a1aab20a1844302cd508b2250efaa6a617ca6d34
Tom FRACHET <tom.frachetdev@gmail.com>
2022-03-11 22:46:53 +0100

    V4 le programme prend en compte les nombres, manque l'interface graphique et le fait de mettre plusieur parenthèse

--- Calcul.py
+++ Calcul.py
@@ -3,7 +3,7 @@ from Resultat import *
 
 
 class Calcul:
-    """Class du calcul que pour un calcul sur des chiffres"""
+    """Class du calcul que pour un calcul sur des nombres"""
 
     def __init__(self, calcul):
         for n in calcul:
@@ -19,17 +19,35 @@ class Calcul:
         self.calcul = calcul
 
     def add_in_list(self):
-        """Ajoute les chiffres et les opérateur dans leurs listes associées"""
+        """Ajoute les nombres et les opérateurs dans leurs listes associées"""
+        nb, coeff, taille, x = 0, 1, [], 0
+        for w in self.calcul:
+            if w == "1" or w == "2" or w == "3" or w == "4" or w == "5" or w == "6" or w == "7" or w == "8" or w == "9" \
+                    or w == "0":
+                x += 1
+            else:
+                taille.append(x)
+                x = 0
+        taille.append(x)
+        x = 0
 
         for i in self.calcul:
             if i == "1" or i == "2" or i == "3" or i == "4" or i == "5" or i == "6" or i == "7" or i == "8" or i == "9" \
                     or i == "0":
-                self.chiffre.append(int(i))
+                coeff = 10 ** (taille[x] - 1)
+                nb += int(i) * coeff
+                taille[x] -= 1
             else:
+
+                self.chiffre.append(nb)
+                nb, coeff = 0, 1
                 self.operator.append(i)
+                x += 1
+        self.chiffre.append(nb)
 
 
 def est_un_chiffre(i: str):
+    """Renvoie True si le le texte est un chiffre"""
     if i == "1" or i == "2" or i == "3" or i == "4" or i == "5" or i == "6" or i == "7" or i == "8" or i == "9" \
             or i == "0":
         return True
--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from Calcul import *
 from Isole_parenthese import *
 from Resultat import *
 
+
 if __name__ == "__main__":
-    c = Calcul("4*(3+2)")
     b = Result("2+3+(2*5)")
-    #b.Result()
     b.affichage()
+
+
